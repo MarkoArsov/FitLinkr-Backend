6af0ad92fb826a2be8f2d910dfd6e49f466b1b91
Marko Arsov <mar@mca.dev>
2024-05-11 16:33:49 +0200

    chore: Add CORS configuration for frontend domain

--- fitlinkr_project/settings.py
+++ fitlinkr_project/settings.py
@@ -143,3 +143,9 @@ import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'data/')
 
 MEDIA_URL = '/data/'
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = (
+    'http://yourfrontenddomain.com',
+)
